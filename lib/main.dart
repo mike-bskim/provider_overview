1f3240e1fdd5cf2c43ecb659ddb45201a69d6d21
mike.bskim <mike.bskim@gmail.com>
2022-05-05 22:18:14 +0200

    19. Provider_Access_anonymous_route_error

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_overview/models/babies.dart';
 
-import 'models/dog.dart';
+import 'models/counter.dart';
+import 'show_me_counter.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final Dog dog = Dog(name: 'dog06', breed: 'breed06', age: 3);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<Dog>(
-          create: (context) => dog,
-        ),
-        // 타입이 같다면 나중에 호출된 값이 화면에 출력된다.
-        // 타입이 다르면 타입에 맞게 출력됨.
-        FutureProvider<int>(
-          create: (context) {
-            final int dogAge = context.read<Dog>().age;
-            final babies = Babies(age: dogAge);
-            return babies.getBabies();
-          },
-          initialData: 0,
-        ),
-        FutureProvider<double>(
-          create: (context) {
-            final int dogAge = context.read<Dog>().age;
-            final babies = Babies(age: dogAge);
-            return babies.getBabies2();
-          },
-          initialData: 0,
-        ),
-        StreamProvider(
-          initialData: 'Bark 0 times',
-          create: (context) {
-            final int dogAge = context.read<Dog>().age;
-            final babies = Babies(age: dogAge + 10);
-            return babies.bark();
-          },
-        )
-      ],
-      child: MaterialApp(
-        title: 'Provider 06 master',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
+    return MaterialApp(
+      title: 'Anonymous Route',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider<Counter>(
+        create: (context) => Counter(),
+        child: const MyHomePage(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
-    debugPrint('_MyHomePageState >> build');
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Provider 06 master'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-              '- name: ${context.read<Dog>().name}',
-              style: const TextStyle(fontSize: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Show Me Counter',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) {
+                    // ChangeNotifierProvider.value 제거할것.
+                    // push 의 context 와 builder 의 context 충돌. builder 의 context 제거할것.
+                    return const ShowMeCounter();
+                  }),
+                );
+              },
             ),
-            const SizedBox(height: 10.0),
-            const BreedAndAge(),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: const Text(
+                'Increment Counter',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              onPressed: () {
+                context.read<Counter>().increment();
+              },
+            )
           ],
         ),
       ),
-    );
-  }
-}
-
-class BreedAndAge extends StatelessWidget {
-  const BreedAndAge({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('BreedAndAge >> build');
-
-    return Column(
-      children: [
-        Text(
-          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
-          style: const TextStyle(fontSize: 20.0),
-        ),
-        const SizedBox(height: 10.0),
-        const Age(),
-      ],
-    );
-  }
-}
-
-class Age extends StatelessWidget {
-  const Age({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('Age >> build');
-
-    return Column(
-      children: [
-        Text(
-          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
-          style: const TextStyle(fontSize: 20.0),
-        ),
-        const SizedBox(height: 10.0),
-        Text(
-          '- number of babies(int): ${context.watch<int>()}',
-          style: const TextStyle(fontSize: 20.0),
-        ),
-        const SizedBox(height: 10.0),
-        Text(
-          '- number of babies(double): ${context.watch<double>()}',
-          style: const TextStyle(fontSize: 20.0),
-        ),
-        const SizedBox(height: 10.0),
-        Text(
-          'Bark - ${context.watch<String>()}',
-          style: const TextStyle(fontSize: 20.0),
-        ),
-        const SizedBox(height: 20.0),
-        ElevatedButton(
-          onPressed: () => context.read<Dog>().grow(),
-          child: const Text(
-            'Grow',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-      ],
     );
   }
 }
